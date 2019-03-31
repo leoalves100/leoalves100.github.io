23ed704b45c0378f5f790c2f3ccab8affea0fc79
Leandro Alves <leandro.alves351@gmail.com>
2019-03-31 16:46:20 -0300

    Usando Service Workers

---
+++ sw.js
@@ -0,0 +1,33 @@
+ // Use a cacheName for cache versioning
+var cacheName = 'v1:static';
+
+// Durante a fase de instalação, você geralmente deseja armazenar em cache os ativos estáticos.
+self.addEventListener('install', function(e) {
+    // Once the service worker is installed, go ahead and fetch the resources to make this work offline.
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            return cache.addAll([
+                './',
+                './offline.html',
+                './main.js'
+            ]).then(function() {
+                self.skipWaiting();
+            });
+        })
+    );
+});
+
+// when the browser fetches a URL…
+self.addEventListener('fetch', function(event) {
+    // … either respond with the cached object or go ahead and fetch the actual URL
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            if (response) {
+                // retrieve from cache
+                return response;
+            }
+            // fetch as normal
+            return fetch(event.request);
+        })
+    );
+});
