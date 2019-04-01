aef7ccd2fc307c45bb39f9b9c62619041a0674c2
Leandro Alves <Leandro.alves351@gmail.com>
2019-04-01 03:01:26 -0300

    Novo arquivo de cache

--- sw.js
+++ sw.js
@@ -12,6 +12,7 @@ self.addEventListener('install', function(e) {
                 '/',
                 '/offline.html',
                 '/main.js',
+                '/sw.js',
                 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css',
                 'https://code.jquery.com/jquery-3.3.1.slim.min.js',
                 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js',
